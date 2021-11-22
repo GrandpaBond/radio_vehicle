400d051715edc39c56ce97a4d85b81d31dd728b4
GrandpaBond <89019204+GrandpaBond@users.noreply.github.com>
2021-11-22 17:26:02 +0000

    some renames

--- main.ts
+++ main.ts
@@ -33,10 +33,10 @@ function alarm () {
 }
 function heatmap (lo: number, val: number, hi: number) {
     mapped = Math.map(val, lo, hi, -320, 320)
-    B = Math.trunc(Math.max(0, 255 - Math.abs(-192 - mapped)))
-    G = Math.trunc(Math.max(0, 255 - Math.abs(64 - mapped)))
-    R2 = 255 - B - G
-    return (R2 * 256 + G) * 256 + B
+    B_byte = Math.trunc(Math.max(0, 255 - Math.abs(-192 - mapped)))
+    G_byte = Math.trunc(Math.max(0, 255 - Math.abs(64 - mapped)))
+    R_byte = 255 - B_byte - G_byte
+    return (R_byte * 256 + G_byte) * 256 + B_byte
 }
 input.onButtonPressed(Button.AB, function () {
     targetRightMotor = 1 - targetRightMotor
@@ -97,14 +97,14 @@ radio.onReceivedValue(function (name, value) {
             Kitronik_Move_Motor.soundSiren(Kitronik_Move_Motor.OnOffSelection.Off)
         }
     } else if (name == "Indicate") {
-        for (let index2 = 0; index2 <= 4; index2++) {
+        for (let flash = 0; flash <= 4; flash++) {
             Indicator(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Orange), value)
             Indicator(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Black), value)
         }
     } else if (name == "Claw") {
-        Claw = Math.map(value, -120, 120, 0, 180)
-        move_motor_zip.setZipLedColor(2, heatmap(0, Claw, 1023))
-        move_motor_zip.setZipLedColor(3, heatmap(0, Claw, 1023))
+        Gape = Math.map(value, -120, 120, 0, 180)
+        move_motor_zip.setZipLedColor(2, heatmap(0, Gape, 1023))
+        move_motor_zip.setZipLedColor(3, heatmap(0, Gape, 1023))
     } else {
         alarm()
     }
@@ -119,27 +119,20 @@ function flex_klaw (count: number) {
     }
 }
 let Gape = 0
-let R2 = 0
-let G = 0
-let B = 0
+let R_byte = 0
+let G_byte = 0
+let B_byte = 0
 let mapped = 0
-let targetRightMotor = 0
-let Claw = 0
 let R_speed = 0
 let L_speed = 0
-let time_out = 0
+let targetRightMotor = 0
 let move_motor_zip: Kitronik_Move_Motor.MoveMotorZIP = null
-move_motor_zip = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
-flex_klaw(3)
-time_out = 0
-L_speed = 0
-R_speed = 0
-Claw = 0
+let time_out = 0
 radio.setGroup(99)
 setup()
 flex_klaw(2)
-time_out = input.runningTime() + 2000
 basic.showIcon(IconNames.Heart)
+time_out = input.runningTime() + 10000
 basic.forever(function () {
     if (input.runningTime() < time_out) {
         set_klaw(Gape)
